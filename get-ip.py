4e1c97d36da0b53f6ba082823279a8f5d2e8f771
Divyesh_Sarvaiya <152177498+Iam-Divyesh@users.noreply.github.com>
2025-08-14 16:41:11 +0530

    Update get-ip.py

--- get-ip.py
+++ get-ip.py
@@ -1,23 +1,24 @@
+import socket
 from flask import Flask, request, jsonify
-from datetime import datetime
-
 
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    return jsonify({"status": "ok", "message": "HeadsIn API is running", "timestamp": datetime.now().isoformat()})
+      return jsonify({"status": "ok", "message": "HeadsIn API is running"})
+    
 
-@app.route("/get-ip", methods=["GET"])
+@app.route('/get-ip', methods=['GET'])
 def get_ip():
-    # Check if behind a proxy/load balancer
-    if request.headers.get("X-Forwarded-For"):
-        ip = request.headers.get("X-Forwarded-For").split(',')[0]  # Take the first IP
-    else:
-        ip = request.remote_addr  # Direct connection
+    host_name = socket.gethostname()
 
-    return jsonify({"ip": ip})
+    ip_address = socket.gethostbyname(host_name)
 
-if __name__ == "__main__":
+    print(host_name)
+    print(ip_address)
+
+    return jsonify({'ip': ip_address, 'hostname': host_name})
+
+if __name__ == '__main__':
     app.run(debug=True)
